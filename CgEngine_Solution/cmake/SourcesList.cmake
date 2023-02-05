d2fd273d3cac96e13d9636548997154ce35cd41b
Leo Preissegger <e12122526@student.tuwien.ac.at>
2023-02-05 15:37:29 +0100

    init

---
+++ CgEngine_Solution/cmake/SourcesList.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION 3.22)
+set(Header_Files
+)
+source_group("Header Files" FILES ${Header_Files})
+set(Source_Files
+    "src/Main.cpp"
+)
+source_group("Source Files" FILES ${Source_Files})
+set(All_Sources ${Header_Files} ${Source_Files})
